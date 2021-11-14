5601256a65f00f790e42fdcf23704ccf39efa590
Antonio Zea Jr <zeajantonio@gmail.com>
2021-11-14 14:30:18 -0500

    Starting queries

--- Project 3/SQL_queries.sql
+++ Project 3/SQL_queries.sql
@@ -1,1 +1,3 @@
-SELECT 
+select ao.Title, ao.Year, pa.DrawnOn, ao.Artist, a.CountryOfOrigin
+FROM (((ART_OBJECT ao join PAINTING pa USING id_no) join EXHIBITS e USING id_no) join EXHIBITIONS ex ON e.Name=ex.Name)
+WHERE ex.Name IN ('Monrning Mist');
