86a07f0db2febc909fafd6df3ec7bde44ab01bd5
Antonio Zea Jr <zeajantonio@gmail.com>
2021-11-14 12:32:59 -0500

    Finished schema

--- Project 3/Art_Museum.sql
+++ Project 3/Art_Museum.sql
@@ -1,93 +1,93 @@
 CREATE TABLE ARTIST (
-    Name VARCHAR(20) NOT NULL,
+    Name VARCHAR(50) NOT NULL,
     DateBorn DATE,
     DateDied DATE,
-    Epoch VARCHAR(20),
-    CountryOfOrigin VARCHAR(20),
-    MainStyle VARCHAR(20),
+    Epoch VARCHAR(50),
+    CountryOfOrigin VARCHAR(50),
+    MainStyle VARCHAR(50),
     Description VARCHAR(200),
     PRIMARY KEY(Name),
     UNIQUE(Name)
 );
 CREATE TABLE ART_OBJECT (
     id_no INT NOT NULL,
-    Title VARCHAR(50),
+    Title VARCHAR(100),
     Description VARCHAR(200),
     Year INT,
-    Artist VARCHAR(20) NOT NULL,
+    Artist VARCHAR(50) NOT NULL,
     PRIMARY KEY(id_no),
     UNIQUE(id_no),
     FOREIGN KEY (Artist) REFERENCES ARTIST(Name)
 );
 CREATE TABLE PAINTING(
     id_no INT NOT NULL,
-    Style VARCHAR(20),
-    PaintType VARCHAR(20),
-    DrawnOn VARCHAR(20),
+    Style VARCHAR(50),
+    PaintType VARCHAR(50),
+    DrawnOn VARCHAR(50),
     PRIMARY KEY(id_no),
     FOREIGN KEY(id_no) REFERENCES ART_OBJECT(id_no)
 );
 CREATE TABLE SCULPTURE(
     id_no INT NOT NULL,
-    Style VARCHAR(20),
+    Style VARCHAR(50),
     Weight FLOAT,
     Height FLOAT,
-    Material VARCHAR(20),
+    Material VARCHAR(50),
     PRIMARY KEY(id_no),
     FOREIGN KEY(id_no) REFERENCES ART_OBJECT(id_no)
 );
 CREATE TABLE STATUE(
     id_no INT NOT NULL,
-    Style VARCHAR(20),
+    Style VARCHAR(50),
     Weight FLOAT,
     Height FLOAT,
-    Material VARCHAR(20),
+    Material VARCHAR(50),
     PRIMARY KEY(id_no),
     FOREIGN KEY(id_no) REFERENCES ART_OBJECT(id_no)
 );
 CREATE TABLE OTHER(
     id_no INT NOT NULL,
-    Style VARCHAR(20),
+    Style VARCHAR(50),
     Weight FLOAT,
     PRIMARY KEY(id_no),
     FOREIGN KEY(id_no) REFERENCES ART_OBJECT(id_no)
 );
 CREATE TABLE COLLECTIONS (
-    Name VARCHAR(20) NOT NULL,
-    Type VARCHAR(20),
+    Name VARCHAR(50) NOT NULL,
+    Type VARCHAR(50),
     Description VARCHAR(200),
-    Address VARCHAR(20),
-    Phone VARCHAR(20),
-    ContactPerson VARCHAR(20),
+    Address VARCHAR(50),
+    Phone VARCHAR(50),
+    ContactPerson VARCHAR(50),
     id_no INT NOT NULL,
-    PRIMARY KEY(Name),
+    PRIMARY KEY(Name, id_no),
     FOREIGN KEY(id_no) REFERENCES ART_OBJECT(id_no)
 );
 CREATE TABLE BORROWED(
-    Collection VARCHAR(20) NOT NULL,
+    Collection VARCHAR(50) NOT NULL,
     DateBorrowed DATE,
     DateReturned DATE,
     PRIMARY KEY(Collection),
     FOREIGN KEY (Collection) REFERENCES COLLECTIONS(Name)
 );
 CREATE TABLE PERMANENT_COLLECTION(
-    Collection VARCHAR(20) NOT NULL,
+    Collection VARCHAR(50) NOT NULL,
     DateAquired DATE,
-    Status VARCHAR(20),
+    Status VARCHAR(50),
     Cost FLOAT,
     PRIMARY KEY(Collection),
     FOREIGN KEY (Collection) REFERENCES COLLECTIONS(Name)
 );
 CREATE TABLE EXHIBITIONS(
-    Name VARCHAR(20) NOT NULL,
+    Name VARCHAR(50) NOT NULL,
     StartDate DATE,
     EndDate DATE,
     PRIMARY KEY(Name)
 );
 CREATE TABLE EXHIBITS(
     id_no INT NOT NULL,
-    Name VARCHAR(20) NOT NULL,
-    PRIMARY KEY(id_no),
+    Name VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id_no, Name),
     FOREIGN KEY(id_no) REFERENCES ART_OBJECT(id_no),
     FOREIGN KEY(Name) REFERENCES EXHIBITIONS(Name)
 );
@@ -101,24 +101,15 @@ INSERT INTO ARTIST (
         Description
     )
 VALUES (
-        'Artist1',
+        'Monet',
         '1840-11-14',
         '1926-12-05',
         'Epoch1',
         'Country1',
         'MainStyle1',
         'Artist1 Description'
-    );
-INSERT INTO ARTIST (
-        Name,
-        DateBorn,
-        DateDied,
-        Epoch,
-        CountryOfOrigin,
-        MainStyle,
-        Description
-    )
-VALUES (
+    ),
+    (
         'Artist2',
         '1776-08-11',
         '1810-03-10',
@@ -126,17 +117,8 @@ VALUES (
         'Country2',
         'MainStyle2',
         'Artist2 Description'
-    );
-INSERT INTO ARTIST (
-        Name,
-        DateBorn,
-        DateDied,
-        Epoch,
-        CountryOfOrigin,
-        MainStyle,
-        Description
-    )
-VALUES (
+    ),
+    (
         'Artist3',
         '1981-08-09',
         '2023-04-11',
@@ -144,17 +126,8 @@ VALUES (
         'Country3',
         'MainStyle3',
         'Artist3 Description'
-    );
-INSERT INTO ARTIST (
-        Name,
-        DateBorn,
-        DateDied,
-        Epoch,
-        CountryOfOrigin,
-        MainStyle,
-        Description
-    )
-VALUES (
+    ),
+    (
         'Artist4',
         '1982-09-10',
         '2024-05-12',
@@ -162,17 +135,8 @@ VALUES (
         'Country4',
         'MainStyle4',
         'Artist4 Description'
-    );
-INSERT INTO ARTIST (
-        Name,
-        DateBorn,
-        DateDied,
-        Epoch,
-        CountryOfOrigin,
-        MainStyle,
-        Description
-    )
-VALUES (
+    ),
+    (
         'Artist5',
         '1983-10-11',
         '2025-06-01',
@@ -180,17 +144,8 @@ VALUES (
         'Country5',
         'MainStyle5',
         'Artist5 Description'
-    );
-INSERT INTO ARTIST (
-        Name,
-        DateBorn,
-        DateDied,
-        Epoch,
-        CountryOfOrigin,
-        MainStyle,
-        Description
-    )
-VALUES (
+    ),
+    (
         'Artist6',
         '1984-11-12',
         '2026-07-01',
@@ -198,17 +153,8 @@ VALUES (
         'Country6',
         'MainStyle6',
         'Artist6 Description'
-    );
-INSERT INTO ARTIST (
-        Name,
-        DateBorn,
-        DateDied,
-        Epoch,
-        CountryOfOrigin,
-        MainStyle,
-        Description
-    )
-VALUES (
+    ),
+    (
         'Artist7',
         '1986-02-02',
         '2028-09-03',
@@ -224,70 +170,188 @@ VALUES(
         'ArtObject1 Description',
         1890,
         'Monet'
-    );
-INSERT INTO ART_OBJECT (id_no, Title, Description, Year, Artist)
-VALUES(
+    ),
+    (
         2,
         'Statue of Liberty',
         'ArtObject2 Description',
         1886,
         'Artist2'
-    );
-INSERT INTO ART_OBJECT (id_no, Title, Description, Year, Artist)
-VALUES(
+    ),
+    (
         3,
         'David',
         'ArtObject3 Description',
         1501,
         'Artist3'
-    );
-INSERT INTO ART_OBJECT (id_no, Title, Description, Year, Artist)
-VALUES(
+    ),
+    (
         4,
         'Cloud Gate',
         'ArtObject4 Description',
         2006,
         'Artist4'
-    );
-INSERT INTO ART_OBJECT (id_no, Title, Description, Year, Artist)
-VALUES(
+    ),
+    (
         5,
         'The Town of Boston in New England',
         'ArtObject5 Description',
         1722,
         'Artist5'
-    );
-INSERT INTO ART_OBJECT (id_no, Title, Description, Year, Artist)
-VALUES(
+    ),
+    (
         6,
         'Sistine Chapel',
         'ArtObject6 Description',
         1473,
         'Artist6'
-    );
-INSERT INTO ART_OBJECT (id_no, Title, Description, Year, Artist)
-VALUES(
+    ),
+    (
         7,
         'The two fridas',
         'ArtObject7 Description',
         1939,
         'Artist7'
-    );
-INSERT INTO ART_OBJECT (id_no, Title, Description, Year, Artist)
-VALUES(
+    ),
+    (
         8,
         'The Persistence of Memory',
         'ArtObject8 Description',
         1931,
-        'Artist1'
-    );
-INSERT INTO ART_OBJECT (id_no, Title, Description, Year, Artist)
-VALUES(
+        'Monet'
+    ),
+    (
         9,
         'The Disintegration of the Persistence of Memory',
         'ArtObject9 Description',
         1952,
         'Artist2'
+    ),
+    (
+        10,
+        'The Green Wave',
+        'ArtObject10 Description',
+        1866,
+        'Monet'
+    ),
+    (
+        11,
+        'Mona Lisa',
+        'ArtObject11 Description',
+        1808,
+        'Artist2'
+    ),
+    (
+        12,
+        'The Starry Night',
+        'ArtObject12 Description',
+        1567,
+        'Artist3'
+    ),
+    (
+        13,
+        'The Last Supper',
+        'ArtObject13 Description',
+        1902,
+        'Artist4'
+    ),
+    (
+        14,
+        'The Scream',
+        'ArtObject14 Description',
+        1911,
+        'Artist5'
+    ),
+    (
+        15,
+        'The Kiss',
+        'ArtObject15 Description',
+        1770,
+        'Artist6'
+    ),
+    (
+        16,
+        'Guernica',
+        'ArtObject16 Description',
+        1452,
+        'Artist7'
+    ),
+    (
+        17,
+        'The Birth of Venus',
+        'ArtObject17 Description',
+        2010,
+        'Monet'
+    ),
+    (
+        18,
+        'The Last Supper',
+        'ArtObject18 Description',
+        1902,
+        'Artist2'
+    ),
+    (
+        19,
+        'Campbells Soup Cans',
+        'ArtObject19 Description',
+        1962,
+        'Artist3'
+    ),
+    (
+        20,
+        'Maman',
+        'ArtObject20 Description',
+        1990,
+        'Artist4'
+    ),
+    (
+        21,
+        'Autumn Rhythm',
+        'ArtObject21 Description',
+        1950,
+        'Artist5'
+    ),
+    (
+        22,
+        'I Still Face You',
+        'ArtObject22 Description',
+        2015,
+        'Artist6'
+    ),
+    (
+        23,
+        'The Physical Impossibility of Death in the Mind of Someone Living',
+        'ArtObject23 Description',
+        1991,
+        'Artist7'
+    ),
+    (
+        24,
+        'The Girl Who Had Everything',
+        'ArtObject24 Description',
+        1998,
+        'Monet'
+    ),
+    (
+        25,
+        'Infinity Mirror Room',
+        'ArtObject25 Description',
+        1965,
+        'Artist2'
+    ),
+    (
+        26,
+        'Untitled',
+        'ArtObject26 Description',
+        1982,
+        'Artist3'
+    ),
+    (
+        27,
+        'Propped',
+        'ArtObject27 Description',
+        1992,
+        'Artist4'
     );
 INSERT INTO PAINTING (id_no, Style, PaintType, DrawnOn)
 VALUES(6, 'Romanticism', 'Fresco', 'Plaster'),
@@ -464,23 +528,47 @@ VALUES(
         'Collection8 Type',
         'Collection8 Collection Description',
         21
+    ),
+    (
+        'Collection9',
+        'Collection9 Type',
+        'Collection9 Collection Description',
+        5
+    ),
+    (
+        'Collection10',
+        'Collection10 Type',
+        'Collection10 Collection Description',
+        4
+    ),
+    (
+        'Collection11',
+        'Collection11 Type',
+        'Collection11 Collection Description',
+        21
+    ),
+    (
+        'Collection12',
+        'Collection12 Type',
+        'Collection12 Collection Description',
+        5
     );
 INSERT INTO BORROWED (Collection, DateBorrowed, DateReturned)
 VALUES('Barnes', '2020-01-01', '2025-01-01'),
-    ('Barnes', '2001-01-01', '2007-01-01'),
-    ('Barnes', '1993-01-01', '1994-01-01'),
-    ('Barnes', '1996-01-01', '1997-01-01'),
     ('Noble', '2018-01-01', '2019-01-01'),
-    ('Noble', '1990-01-01', '1991-01-01'),
-    ('Noble', '2001-01-01', '2002-01-01');
+    ('Full Stop', '2015-01-01', '2020-01-01'),
+    ('Collection9', '2018-01-01', '2019-01-01'),
+    ('Collection10', '2018-01-01', '2019-01-01'),
+    ('Collection11', '2018-01-01', '2019-01-01'),
+    ('Collection12', '2018-01-01', '2019-01-01');
 INSERT INTO PERMANENT_COLLECTION (Collection, DateAquired, Status, Cost)
 VALUES('Homestyle', '1990-01-01', 'display', 120000.00),
-    ('Collection 3', '1991-01-01', 'loan', 1000.00),
-    ('Collection 4', '1996-01-01', 'loan', 140000.00),
-    ('Collection 5', '1996-01-01', 'loan', 140000.00),
-    ('Collection 6', '1996-01-01', 'loan', 140000.00),
-    ('Collection 7', '1996-01-01', 'loan', 140000.00),
-    ('Collection 8', '1996-01-01', 'loan', 140000.00);
+    ('Collection3', '1991-01-01', 'loan', 1000.00),
+    ('Collection4', '1996-01-01', 'loan', 140000.00),
+    ('Collection5', '1996-01-01', 'loan', 140000.00),
+    ('Collection6', '1996-01-01', 'loan', 140000.00),
+    ('Collection7', '1996-01-01', 'loan', 140000.00),
+    ('Collection8', '1996-01-01', 'loan', 140000.00);
 INSERT INTO EXHIBITIONS (Name, StartDate, EndDate)
 VALUES('Exhibition1', '1996-01-01', '1997-01-01'),
     ('Exhibition2', '1993-01-01', '1994-01-01'),
@@ -491,15 +579,15 @@ VALUES('Exhibition1', '1996-01-01', '1997-01-01'),
     ('Monrning Mist', '1999-01-01', '2003-01-01');
 INSERT INTO EXHIBITS (id_no, Name)
 VALUES(1, 'Monrning Mist'),
-(2, 'Monrning Mist'),
-(3, 'Monrning Mist'),
-(4, 'Monrning Mist'),
-(5, 'Monrning Mist'),
-(3, 'Exhibition1'),
-(4, 'Exhibition2'),
-(4, 'Exhibition3'),
-(5, 'Exhibition4'),
-(6, 'Exhibition5'),
-(2, 'Exhibition6'),
-(2, 'Exhibition6'),
-(2, 'Exhibition6');
+    (2, 'Monrning Mist'),
+    (3, 'Monrning Mist'),
+    (4, 'Monrning Mist'),
+    (5, 'Monrning Mist'),
+    (3, 'Exhibition1'),
+    (4, 'Exhibition2'),
+    (4, 'Exhibition3'),
+    (5, 'Exhibition4'),
+    (6, 'Exhibition5'),
+    (2, 'Exhibition6'),
+    (12, 'Exhibition6'),
+    (14, 'Exhibition6');
